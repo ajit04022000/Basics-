c8c0894c037a227d815cc1fa1a42c6e8090ef3bf
Ajit kumar patel <95335216+ajit04022000@users.noreply.github.com>
2021-12-01 14:14:52 +0530

    Add files via upload

---
+++ 6.CHAPTER 6 VS code/01_conditional.py
@@ -0,0 +1,44 @@
+# #1.if-elif-else lader in python
+# a = int(input("print the number\n"))
+# if (a<3) :
+#     print("the value of a is grater than 3")
+
+# elif (a>13):
+#     print("the value of a is grater than 13")
+# elif (a>7) :
+#     print("the value of a is grater than 7")
+# elif (a>17) :
+#     print("the value of a is grater than 17")
+   
+# else:
+#     print("the value is not grater than 3 or 7")
+
+
+# print("done")
+
+
+
+
+# #2.multiple if statement
+# a = int(input("input a number"))
+# if (a<3):
+#     print("the value of a is smaller then 3")
+
+# if (a>13):
+#     print("the value of a is grater thne 13")
+
+# if(a>7):
+#     print("the value of a is grater then 7")
+# else:
+#     print("the value is not grater than 3 or 7")
+
+# print("done")
+
+
+
+#code example
+a = int(input("enter a no"))
+if(a>9):
+    print("greater")
+else:
+    print("smaller")
---
+++ 6.CHAPTER 6 VS code/02_quick_quiz.py
@@ -0,0 +1,5 @@
+age = int(input("enter your age\n"))
+if (age>=18):
+    print("yes")
+else:
+    print("you are out of age")
