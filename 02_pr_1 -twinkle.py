385d02493b21dddda3a1b8f554622259996b33ab
Ajit kumar patel <95335216+ajit04022000@users.noreply.github.com>
2021-12-01 12:02:58 +0530

    Delete 02_pr_1 -twinkle.py

--- 02_pr_1 -twinkle.py
+++
@@ -1,8 +0,0 @@
-# write a program to print twinkle twinkle littel star poem in python
-'''write a program to print twinkle twinkle littel star poem in python'''
-
-
-print('''Twinkle, twinkle, little star,
-How I wonder what you are!
-Up above the world so high,
-Like a diamond in the sky.''' )
