611e816cfb179e1626163e1133cedb365dc66755
Ajit kumar patel <95335216+ajit04022000@users.noreply.github.com>
2021-12-01 12:02:43 +0530

    Delete 01_hello.py

--- 01_hello.py
+++
@@ -1,5 +0,0 @@
-# author : ajit                          #single line COMMENTS
-# Licenced to abc company
-''' thanks for the oppertuanity '''       #multi line COMMENTS
-print ("hello world")
-import os                                  # BUILT IN MODULE
